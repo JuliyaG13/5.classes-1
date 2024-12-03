a068157b26aa7eb9fdeb58c0203bd5d39ed11e4f
Yuliya Gumitova <yulya1397@mail.ru>
2024-12-03 23:23:25 +0300

    added

---
+++ class.js
@@ -0,0 +1,88 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name; 
+        this.releaseDate = releaseDate; 
+        this.pagesCount = pagesCount; 
+        this._state = 100; 
+        this.type = null; 
+    }
+
+    fix() {
+        this.state = this.state * 1.5;
+    }
+
+    set state(value) {
+        if (value < 0) {
+            this._state = 0; 
+        } else if (value > 100) {
+            this._state = 100;
+        } else {
+            this._state = value;
+        }
+    }
+
+    get state() {
+        return this._state; 
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = 'magazine'; 
+    }
+}
+
+// Класс для книг
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author; 
+        this.type = 'book'; 
+    }
+}
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = 'novel'; 
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = 'fantastic'; 
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = 'detective'; 
+    }
+}
+
+const sherlock = new PrintEditionItem(
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+    2019,
+    1008
+);
+
+console.log(sherlock.releaseDate); 
+console.log(sherlock.state); 
+sherlock.fix();
+console.log(sherlock.state); 
+
+const picknick = new FantasticBook(
+    "Аркадий и Борис Стругацкие",
+    "Пикник на обочине",
+    1972,
+    168
+);
+
+console.log(picknick.author); 
+picknick.state = 10;
+console.log(picknick.state); 
+picknick.fix();
+console.log(picknick.state); 
